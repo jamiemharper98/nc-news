601859a570d3a321ae95e26720e661a730ca1b5d
jamiemharper98 <jamiemharper98@gmail.com>
2023-11-20 12:57:07 +0000

    add ability to get the access the endpoints json and dynamically test this json

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const { getTopics } = require("./controllers/topics.controller");
 const { psqlErrors, customErrors, serverErrors, wrongPath } = require("./db/errors");
+const { getApi } = require("./controllers/api.controller");
 const app = express();
 
 app.use(express.json());
 
+app.get("/api", getApi)
+
 app.get("/api/topics", getTopics);
+
 
 app.use("*", wrongPath);
 app.use(psqlErrors);
