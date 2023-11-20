693af8fb6e33a90ce989c6f82ab274214ea34c47
jamiemharper98 <jamiemharper98@gmail.com>
2023-11-20 13:47:44 +0000

    tests written initially ignored

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -3,6 +3,7 @@ const request = require("supertest");
 const db = require("../db/connection");
 const seed = require("../db/seeds/seed");
 const { topicData, userData, articleData, commentData } = require("../db/data/test-data/index");
+require("jest-sorted");
 
 beforeEach(() => seed({ topicData, userData, articleData, commentData }));
 afterAll(() => db.end());
@@ -37,6 +38,28 @@ describe("/api/topics", () => {
           expect(typeof topic.slug).toBe("string");
           expect(typeof topic.description).toBe("string");
         });
+      });
+  });
+});
+
+xdescribe("/api/articles", () => {
+  test("GET:200 responds with an array of article objects", () => {
+    return request(app)
+      .get("/api/articles")
+      .expect(200)
+      .then(({ body: { articles } }) => {
+        articles.forEach((article) => {
+          expect(typeof article.author).toBe("string");
+          expect(typeof article.title).toBe("string");
+          expect(typeof article.article_id).toBe("number");
+          expect(typeof article.topic).toBe("string");
+          expect(typeof article.created_at).toBe("string");
+          expect(typeof article.votes).toBe("number");
+          expect(typeof article.article_img_url).toBe("string");
+          expect(typeof article.comment_count).toBe("number");
+          expect(article.body).toBe(undefined);
+        });
+        expect(articles).toBeSortedBy("created_at", { descending: true });
       });
   });
 });
--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const { getTopics } = require("./controllers/topics.controller");
 const { psqlErrors, customErrors, serverErrors, wrongPath } = require("./db/errors");
+const { getArticles } = require("./controllers/articles.controller");
 const app = express();
 
 app.use(express.json());
 
 app.get("/api/topics", getTopics);
+
+app.get("/api/articles", getArticles)
 
 app.use("*", wrongPath);
 app.use(psqlErrors);
