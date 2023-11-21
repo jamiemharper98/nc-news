87ebe047843341555754249612759d9b7aeeb8c9
jamiemharper98 <jamiemharper98@gmail.com>
2023-11-21 15:27:44 +0000

    add comment count to articles and changed join to allow for a article with no comments to be found

--- models/articles.model.js
+++ models/articles.model.js
@@ -23,13 +23,22 @@ exports.selectArticles = ({ topic }) => {
 };
 
 exports.selectArticleByID = (id) => {
-  return db.query(`SELECT * FROM articles WHERE articles.article_id = $1`, [id]).then(({ rows }) => {
-    if (!rows.length) {
-      return Promise.reject({ status: 404, msg: "Article ID does not exist!" });
-    } else {
-      return rows[0];
-    }
-  });
+  return db
+    .query(
+      `
+  SELECT articles.*, COUNT(comments.comment_id)::INT AS comment_count FROM articles
+  LEFT JOIN comments ON articles.article_id = comments.article_id
+  WHERE articles.article_id = $1
+  GROUP BY articles.article_id`,
+      [id]
+    )
+    .then(({ rows }) => {
+      if (!rows.length) {
+        return Promise.reject({ status: 404, msg: "Article ID does not exist!" });
+      } else {
+        return rows[0];
+      }
+    });
 };
 
 exports.updateArticleByID = (id, { inc_votes }) => {
